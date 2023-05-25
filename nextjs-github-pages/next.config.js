a1dccaab6760df68760d5d6d7d7723f9852c9d45
Jonathan Fisher <jpfisher72@gmail.com>
2023-05-25 10:05:04 -0400

    Initial

---
+++ nextjs-github-pages/next.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: 'export',
+  basePath: '/Test-Next-Deploy-Pages',
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = nextConfig
